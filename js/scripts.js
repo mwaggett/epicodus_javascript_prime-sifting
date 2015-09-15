6f6c266bfbe761c48397d6a8ca23da80be297cd1
Andrea Padgett & Molly Waggett <student@epicodus.com>
2015-09-15 14:23:44 -0700

    Write function that returns an array of prime numbers up to an input number using subfunctions to create an array of every number up to the input number and to check whether one number is a multiple of another.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,30 @@
-// var functionName = function(params) {
-//
-// };
+var primeSift = function(number) {
+  if (number >= 2) {
+    var myArray = arrayUpToNumber(number);
+    for (var i = 0; i < myArray.length - 1; i++) {
+      for (var j = i+1; j < myArray.length; j++) {
+        if (isMultiple(myArray[i],myArray[j])) {
+          myArray.splice(j,1);
+        }
+      }
+    }
+    return myArray;
+  } else {
+    return "No prime numbers."
+  }
+}
+
+var arrayUpToNumber = function(number) {
+  var myArray = [];
+  for(var i = 2; i <= number; i++ ){
+    myArray.push(i);
+  }
+  return myArray;
+};
+
+var isMultiple = function(numberA, numberB){
+  return numberB % numberA === 0;
+}
 
 // $(document).ready(function() {
 //
--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,29 @@
-// describe('functionName', function() {
-//   it("plain English description of spec", function() {
-//     expect(functionName(argument)).to.equal(value);
-//   });
-// });
+describe('arrayUpToNumber', function() {
+  it("creates an array from 2 to input number", function() {
+    expect(arrayUpToNumber(20)).to.eql([2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]);
+  });
+});
+
+describe('isMultiple', function() {
+  it("returns true if B is a mutliple of A", function() {
+    expect(isMultiple(3, 9)).to.equal(true);
+  });
+
+  it("returns false if B is not a multiple of A", function() {
+    expect(isMultiple(3, 10)).to.equal(false);
+  });
+});
+
+describe('primeSift', function() {
+  it("creates an array of primes from 2 to input number", function() {
+    expect(primeSift(30)).to.eql([2,3,5,7,11,13,17,19,23,29]);
+  });
+
+  it("handles low number", function() {
+    expect(primeSift(2)).to.eql([2]);
+  });
+
+  it("yells about numbers less than 2", function() {
+    expect(primeSift(1)).to.equal("No prime numbers.");
+  });
+});
